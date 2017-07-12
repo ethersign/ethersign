5e5bfa35dfc8564a73958766dbd80fc29ad8e7c7
Andy Xubuntu <admazzola@gmail.com>
2017-07-12 13:02:49 -0700

    fixing

--- app/ether-sign.js
+++ app/ether-sign.js
@@ -1,6 +1,6 @@
 var crypto = require('crypto')
 var eth_utils = require('ethereumjs-util');
- 
+
 
 		exports.connectToEthereumUsingProvider = function (_web3_provider)
 		{
@@ -22,36 +22,6 @@ var eth_utils = require('ethereumjs-util');
 
 
 
-		//use web3 ethereum light client to verify that a cryptopunks is owned by a particular public address
-
-		exports.verifyCryptopunkOwnedByPublicAddress = function (_eth, _punk_id,_eth_pub_addr)
-		{
- 			//IMPLEMENT ME
-
-			var cryptopunks_contract_address = "0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB"
-
-			var result = _eth.getStorageAt() //?
-
-		}
-
-
-
-		//use web3 ethereum light client to return a list of cryptopunks at a public address
-
-		exports.getCryptopunksAtPublicAddress = function (_eth_pub_addr)
-		{
-
-		}
-
-		exports.getRandomCryptopunkAtPublicAddress = function (_eth_pub_addr)
-		{
-
-		}
-
-		exports.getFirstCryptopunkAtPublicAddress = function (_eth_pub_addr)
-		{
-
-		}
 
 
 		 //generate a challenge for the user to sign in order to prove ownership of their private key
@@ -136,35 +106,4 @@ var eth_utils = require('ethereumjs-util');
 		}
 
 
-		exports.fixCryptopunkId = function (_icon_id)
-		{
-			while(_icon_id.length < 3)
-			{
-				_icon_id = "0" + _icon_id;
-			}
-
-			return _icon_id;
-		}
-
-		exports.getCryptopunkIconLocalImagePath = function (_icon_id)
-		{
-			_icon_id = exports.fixCryptopunkId(_icon_id)
-
-
-			var fs = require('fs');
-			var path = require('path');
-
-
-			var assets_folder = path.join(path.dirname(fs.realpathSync(__filename)), '../app/assets');
-			var icon_file_path =  (assets_folder + '/punk'+_icon_id+'.png');
-			//var icon_file = fs.readFileSync(icon_file_path);
-
-			return icon_file_path;
-		}
-
-		exports.getCryptopunkIconCentralizedURL = function (_icon_id)
-		{
-			_icon_id = exports.fixCryptopunkId(_icon_id)
-
-			return "https://www.larvalabs.com/cryptopunks/cryptopunk"+_icon_id+".png"
-		}
+	 
